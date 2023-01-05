007c9355e869d84dac862ea9a0983cb6e418a6c8
VijayKumarRatnala99 <118876961+VijayKumarRatnala99@users.noreply.github.com>
2023-01-05 09:34:50 +0530

    Add files via upload

---
+++ Voting.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class  Voting
+{
+	
+	public static void main(String[] args) 
+	{
+	System.out.println("Note : If you are below 18 please kindly ignore this");
+       Scanner sc  = new Scanner(System.in);
+	   System.out.print("Enter your age : ");
+	   int age = sc.nextInt();
+	   
+
+	   if (age >= 18)
+		{
+		   System.out.println("You are eligible for voting purpose ");
+	    }
+
+		else 
+			System.out.println("You are not eligible for voting purpose ");
+
+		System.out.println("Thank you");
+
+      
+	   
+	}
+}
+
+
+
+
+//++55 
+//55++
